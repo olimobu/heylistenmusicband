664810610f0369f2ef044219d9799ff1b0c4af59
olimobu <oliver.moya.bueno@gmail.com>
2019-07-17 23:40:12 +0200

    Web realizada, a la espera de retoques

---
+++ main.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  /* Introducir año actual en la información del Copyright */
+  var date = new Date();
+  var currentYear = date.getFullYear();
+  document.getElementById("currentYear").innerText = currentYear;
+};
